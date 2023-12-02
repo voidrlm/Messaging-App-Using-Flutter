679d72f8dee092249c239538413437d3b44bb1a9
voidrlm <varjun609@gmail.com>
2023-12-03 02:11:50 +0530

    Added online/offline notifiers

--- lib/modules/homeScreen/screen.dart
+++ lib/modules/homeScreen/screen.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  // Dummy data for user list
-  final List<Map<String, String>> userList = [
+  final List<Map<String, dynamic>> userList = [
     {
       'name': 'Tony',
       'lastMessage': 'Hello!',
       'avatar':
-          'https://i.pinimg.com/originals/99/71/23/997123ba1bfc03b01c62848519c6c289.jpg'
+          'https://i.pinimg.com/originals/99/71/23/997123ba1bfc03b01c62848519c6c289.jpg',
+      'status': 'active',
     },
     {
       'name': 'Wayne',
       'lastMessage': 'Hi there!',
       'avatar':
-          'https://www.slashfilm.com/img/gallery/christian-bale-claims-his-pay-for-american-psycho-was-less-than-the-films-make-up-artists/l-intro-1672278324.jpg'
+          'https://www.slashfilm.com/img/gallery/christian-bale-claims-his-pay-for-american-psycho-was-less-than-the-films-make-up-artists/l-intro-1672278324.jpg',
+      'status': 'offline',
     },
     {
       'name': 'Bruce',
       'lastMessage': 'How are you?',
-      'avatar': 'https://i.insider.com/5de6ddd479d7571d25446737?width=700'
+      'avatar': 'https://i.insider.com/5de6ddd479d7571d25446737?width=700',
+      'status': 'active',
     },
     // Add more users as needed
   ];
@@ -26,23 +28,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Friends'),
+        title: Text('User List'),
       ),
       body: ListView.builder(
         itemCount: userList.length,
         itemBuilder: (context, index) {
           return ListTile(
             leading: CircleAvatar(
-              backgroundImage: NetworkImage(userList[index]['avatar']!),
+              backgroundImage: NetworkImage(userList[index]['avatar']),
             ),
-            title: Text(userList[index]['name']!),
-            subtitle: Text(userList[index]['lastMessage']!),
+            title: Row(
+              children: [
+                Text(userList[index]['name']),
+                SizedBox(width: 8),
+                _buildStatusNotifier(userList[index]['status']),
+              ],
+            ),
+            subtitle: Text(userList[index]['lastMessage']),
             onTap: () {
-              print('${userList[index]['name']}');
+              // Handle tapping on a user, e.g., navigate to a chat screen
+              // For simplicity, you can print a message for now
+              print('Tapped on ${userList[index]['name']}');
             },
           );
         },
+      ),
+    );
+  }
+
+  Widget _buildStatusNotifier(String status) {
+    return Container(
+      width: 10,
+      height: 10,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: status == 'active'
+            ? Colors.green
+            : const Color.fromARGB(255, 121, 121, 121),
       ),
     );
   }
