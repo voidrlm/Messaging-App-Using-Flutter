566d6bf3ff1f6a98b20c5794afba73664b336eee
voidrlm <varjun609@gmail.com>
2023-12-03 02:07:29 +0530

    Added friends list view in home page

--- lib/modules/homeScreen/screen.dart
+++ lib/modules/homeScreen/screen.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  // Dummy data for user list
+  final List<Map<String, String>> userList = [
+    {
+      'name': 'Tony',
+      'lastMessage': 'Hello!',
+      'avatar':
+          'https://i.pinimg.com/originals/99/71/23/997123ba1bfc03b01c62848519c6c289.jpg'
+    },
+    {
+      'name': 'Wayne',
+      'lastMessage': 'Hi there!',
+      'avatar':
+          'https://www.slashfilm.com/img/gallery/christian-bale-claims-his-pay-for-american-psycho-was-less-than-the-films-make-up-artists/l-intro-1672278324.jpg'
+    },
+    {
+      'name': 'Bruce',
+      'lastMessage': 'How are you?',
+      'avatar': 'https://i.insider.com/5de6ddd479d7571d25446737?width=700'
+    },
+    // Add more users as needed
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Welcome!',
-          style: TextStyle(
-            fontSize: 24,
-            color: Color.fromARGB(255, 10, 10, 10),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Friends'),
+      ),
+      body: ListView.builder(
+        itemCount: userList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(userList[index]['avatar']!),
+            ),
+            title: Text(userList[index]['name']!),
+            subtitle: Text(userList[index]['lastMessage']!),
+            onTap: () {
+              print('${userList[index]['name']}');
+            },
+          );
+        },
       ),
     );
   }
