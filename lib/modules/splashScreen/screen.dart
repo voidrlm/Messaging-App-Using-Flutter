e3e692fd4cbb6550d2cdea028fe6df3205689cf5
voidrlm <varjun609@gmail.com>
2023-12-03 01:34:31 +0530

    Initial splash screen

--- lib/modules/splashScreen/screen.dart
+++ lib/modules/splashScreen/screen.dart
@@ -1,1 +1,47 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 1),
+      () => Navigator.pushReplacementNamed(context, '/home'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.message,
+              size: 100,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Messages',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 8),
+          ],
+        ),
+      ),
+    );
+  }
+}
